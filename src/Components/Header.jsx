2903e819828c9529dfdabb94e48e13a4011ce0a2
Wallysson Cavalcante <wallysson.cavalcante@gmail.com>
2025-08-14 21:15:18 -0300

    Feat: tela inicial, cadastro, esqueci a senha, e tela de carregamento

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
-import Dogs from "../Assets/dogs.svg?react";
-import { UserContext } from "../UserContext";
+import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import Dogs from '../Assets/dogs.svg?react';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
-  const { data, userLogout } = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
@@ -15,7 +16,6 @@ const Header = () => {
         {data ? (
           <Link className={styles.login} to="/conta">
             {data.nome}
-            <button onClick={userLogout}>Sair</button>
           </Link>
         ) : (
           <Link className={styles.login} to="/login">
